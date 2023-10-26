b14d9b7e67b6997115d0d560f4b13acc8691f56c
Zbiku <Zbiku@MSI>
2023-10-26 22:47:33 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        double oceny;
+        Console.WriteLine("Podaj swoją średnią ocen: ");
+        oceny = double.Parse(Console.ReadLine());
+        if(oceny>=2.00 && oceny<=3.99)
+        {
+            Console.WriteLine("Przysługiwane stypendium wynosi 0,00 zł");
+        }
+        else if (oceny>=4.0 && oceny<=4.79)
+        {
+            Console.WriteLine("Przysługiwane stypendium wynosi 350,00 zł");
+        }
+        else if (oceny >= 4.80 && oceny <= 5.00)
+        {
+            Console.WriteLine("Przysługiwane stypendium wynosi 550,00 zł");
+        }
+        else
+        {
+            Console.WriteLine("Podano błędną średnią ocen");
+        }
+        Console.ReadKey();
+    }
+}
